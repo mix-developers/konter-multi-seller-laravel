0b9905e748d1d90843470906a296fa043e5264ff
firchun025 <firchun025@gmail.com>
2023-07-26 08:04:57 +0900

    update layout

--- resources/views/layouts/auth_theme/app.blade.php
+++ resources/views/layouts/auth_theme/app.blade.php
@@ -8,9 +8,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="">
-    <meta name="author" content="Phoenixcoded" />
+    <meta name="description" content="SimVice website multi konter">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="Farsiah Irmawati">
+    <meta name="generator" content=" Mix Developers">
+    <meta name="keywords" content="e-konter">
 
     <!-- Favicon icon -->
     <link rel="icon" href="{{ asset('/') }}img/icon.png" type="image/x-icon">
--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,68 +1,9 @@
 <!--footer-main-->
 <footer class="footer-main">
-    <div class="footer-top">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <div class="about-widget">
-                        <div class="footer-logo">
-                            <figure>
-                                <a href="index.html">
-                                    <img loading="lazy" class="img-fluid" src="images/logo-2.png" alt="medic">
-                                </a>
-                            </figure>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, temporibus?</p>
-                        <ul class="location-link">
-                            <li class="item">
-                                <i class="fas fa-map-marker-alt"></i>
-                                <p>Modamba, NY 80021, United States</p>
-                            </li>
-                            <li class="item">
-                                <i class="far fa-envelope" aria-hidden="true"></i>
-                                <a href="mailto:support@medic.com">
-                                    <p>support@medic.com</p>
-                                </a>
-                            </li>
-                            <li class="item">
-                                <i class="fas fa-phone" aria-hidden="true"></i>
-                                <p>(88017) +123 4567</p>
-                            </li>
-                        </ul>
-                        <ul class="list-inline social-icons">
-                            <li class="list-inline-item"><a href="https://facebook.com/themefisher"
-                                    aria-label="facebook"><i class="fab fa-facebook-f"></i></a></li>
-                            <li class="list-inline-item"><a href="https://twitter.com/themefisher"
-                                    aria-label="twitter"><i class="fab fa-twitter"></i></a></li>
-                            <li class="list-inline-item"><a href="https://instagram.com/themefisher"
-                                    aria-label="instagram"><i class="fab fa-instagram"></i></a></li>
-                            <li class="list-inline-item"><a href="https://github.com/themefisher" aria-label="github"><i
-                                        class="fab fa-github"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-5 mb-3 mb-md-0">
-                    <h2>Layanan Kami</h2>
-                    <ul class="menu-link">
-
-                        <li>
-                            <a href="{{ url('/') }}">
-                                <i class="fa fa-angle-right" aria-hidden="true"></i>Bantuan</a>
-                        </li>
-                        <li>
-                            <a href="{{ url('/') }}">
-                                <i class="fa fa-angle-right" aria-hidden="true"></i>Garansi</a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-    </div>
     <div class="footer-bottom">
         <div class="container clearfix">
             <div class="copyright-text">
-                <p>&copy; Copyright {{ date('Y') }} .
+                <p>&copy; Copyright {{ date('Y') }} . <a href="{{ url('/') }}">SimVice</a>
                 </p>
             </div>
             <ul class="footer-bottom-link">
