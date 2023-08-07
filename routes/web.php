6fc49bbe9964624e84ad464f966dc12c7d14d11c
firchun025 <firchun025@gmail.com>
2023-08-07 09:35:31 +0900

    update route user

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KonterController;
 use App\Http\Controllers\KonterTokoController;
